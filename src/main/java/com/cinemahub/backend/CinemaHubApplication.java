4a52a7edf68597aa1c0a8ea9cfb1e3e0bd576154
Bartek <bartek13@vip.onet.pl>
2024-04-10 23:40:15 +0200

    first commit

---
+++ src/main/java/com/cinemahub/backend/CinemaHubApplication.java
@@ -0,0 +1,13 @@
+package com.cinemahub.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinemahub/backend/CinemaHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinemahub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
